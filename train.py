4fb5410c5a49d4525aa65453ef6de2c677812d2c
qianyue996 <3010875645@qq.com>
2025-03-24 19:55:42 +0800

    update

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ def main():
     except Exception as e:
         pass
 
-    model=Yolov1(S, C).to(device).half()
+    model=Yolov1(S, C).to(device)
     if checkpoint:
         model.load_state_dict(checkpoint['model'])
         
@@ -53,46 +53,48 @@ def main():
     losses=[]
     for epoch in range(500):
         batch_avg_loss=0
-        for batch_x,batch_y in tqdm(dataloader):
-            batch_x,batch_y=batch_x.to(device).half(),batch_y.to(device).half()
-            batch_output=model(batch_x)
-            
-            loss=torch.tensor(0)
-            for i in range(len(batch_x)):
-                x=batch_x[i]
-                y=batch_y[i]
-                output=batch_output[i]
-                # foreach grid 
-                for row in range(S):    
-                    for col in range(S):
-                        pred_grid=output[row,col] 
-                        target_grid=y[row,col]
-                        if not target_grid[4]>0: # no object in this grid
-                            loss_c_noobj=(pred_grid[4])**2+(pred_grid[9])**2 # no object in grid,so target c is 0
-                            loss=loss+LAMBDA_NOOBJ*loss_c_noobj
-                            continue 
-                        # IOU
-                        iou_bbox1=compute_iou(row,col,pred_grid[:4],target_grid[:4],IMG_SIZE,S)
-                        iou_bbox2=compute_iou(row,col,pred_grid[5:9],target_grid[:4],IMG_SIZE,S)
-                        # 取IOU大的预测框的x,y,w,h,c
-                        if iou_bbox1>iou_bbox2:
-                            xywh=pred_grid[:4]
-                            c_obj,c_noobj=pred_grid[4],pred_grid[9]
-                            iou_obj,iou_noobj=iou_bbox1,iou_bbox2
-                        else:
-                            xywh=pred_grid[5:9]
-                            c_obj,c_noobj=pred_grid[9],pred_grid[4]
-                            iou_obj,iou_noobj=iou_bbox2,iou_bbox1
-                        loss_xywh=(xywh[0]-target_grid[0])**2+(xywh[1]-target_grid[1])**2+(torch.sqrt(xywh[2])-torch.sqrt(target_grid[2]))**2+(torch.sqrt(xywh[3])-torch.sqrt(target_grid[3]))**2
-                        loss_c_obj=(c_obj-iou_obj)**2
-                        loss_c_noobj=c_noobj**2
-                        loss_class=((pred_grid[10:]-target_grid[10:])**2).sum()
-                        loss=loss+loss_xywh*LAMBDA_COORD+loss_c_obj+loss_c_noobj*LAMBDA_NOOBJ+loss_class
-            loss=loss/len(batch_x)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            batch_avg_loss+=loss.item()
+        with tqdm(dataloader) as bar:
+            for batch_x,batch_y in bar:
+                batch_x,batch_y=batch_x.to(device),batch_y.to(device)
+                batch_output=model(batch_x)
+                
+                loss=torch.tensor(0)
+                for i in range(len(batch_x)):
+                    x=batch_x[i]
+                    y=batch_y[i]
+                    output=batch_output[i]
+                    # foreach grid 
+                    for row in range(S):    
+                        for col in range(S):
+                            pred_grid=output[row,col] 
+                            target_grid=y[row,col]
+                            if not target_grid[4]>0: # no object in this grid
+                                loss_c_noobj=(pred_grid[4])**2+(pred_grid[9])**2 # no object in grid,so target c is 0
+                                loss=loss+LAMBDA_NOOBJ*loss_c_noobj
+                                continue
+                            # IOU
+                            iou_bbox1=compute_iou(row,col,pred_grid[:4],target_grid[:4],IMG_SIZE,S)
+                            iou_bbox2=compute_iou(row,col,pred_grid[5:9],target_grid[:4],IMG_SIZE,S)
+                            # 取IOU大的预测框的x,y,w,h,c
+                            if iou_bbox1>iou_bbox2:
+                                xywh=pred_grid[:4]
+                                c_obj,c_noobj=pred_grid[4],pred_grid[9]
+                                iou_obj,iou_noobj=iou_bbox1,iou_bbox2
+                            else:
+                                xywh=pred_grid[5:9]
+                                c_obj,c_noobj=pred_grid[9],pred_grid[4]
+                                iou_obj,iou_noobj=iou_bbox2,iou_bbox1
+                            loss_xywh=(xywh[0]-target_grid[0])**2+(xywh[1]-target_grid[1])**2+(torch.sqrt(xywh[2])-torch.sqrt(target_grid[2]))**2+(torch.sqrt(xywh[3])-torch.sqrt(target_grid[3]))**2
+                            loss_c_obj=(c_obj-iou_obj)**2
+                            loss_c_noobj=c_noobj**2
+                            loss_class=((pred_grid[10:]-target_grid[10:])**2).sum()
+                            loss=loss+loss_xywh*LAMBDA_COORD+loss_c_obj+loss_c_noobj*LAMBDA_NOOBJ+loss_class
+                loss=loss/len(batch_x)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                batch_avg_loss+=loss.item()
+                bar.set_postfix({'loss:':loss.item()})
         
         batch_avg_loss=batch_avg_loss/len(dataloader)
         losses.append(batch_avg_loss)
